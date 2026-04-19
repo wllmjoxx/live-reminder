e9d9e36e63f6ad1a02d6ad3d941272a63d39e5ca
wllmjoxx <wllmjonaa@gmail.com>
2026-04-19 23:59:53 +0700

    asdasdas

--- app.js
+++ app.js
@@ -1188,6 +1188,7 @@ function renderHariH(data, formResponses = []) {
   html += `
     <div style="text-align:center;margin-bottom:12px">
       <div style="font-size:0.85rem;font-weight:700;color:var(--bs-dark)">📅 Data Hari H (Belum diisi)</div>
+      <div style="font-size:0.85rem;font-weight:700;color:var(--bs-dark)">!Harap selalu cek manual jam pada foto file GMV host!</div>
       <div style="font-size:0.68rem;color:var(--bs-muted);margin-top:3px">${data.date}</div>
     </div>`;
 
