8fabcc0e930fc60cc18edcf00342c8cd32af2f02
BGJOXX <wllmjonaa@gmail.com>
2026-05-09 20:14:18 +0700

    asdasdsadad

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "live-reminder-v8";
+const CACHE_NAME = "live-reminder-v10";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png"];
 
 self.addEventListener("install", e => {
