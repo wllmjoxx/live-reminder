07ac68cb12dbcae3dc7a9de751f88d55fd33dba7
wllmjoxx <wllmjonaa@gmail.com>
2026-09-25 12:02:35 +0700

    asdsdad

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "live-reminder-v12";
+const CACHE_NAME = "live-reminder-v15";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png"];
 
 self.addEventListener("install", e => {
